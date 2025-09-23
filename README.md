53fdf33cb18918e5aada51b5722e12bd3bfcc029
JaimeDevCode <devjaimesevilla@gmail.com>
2025-09-23 19:40:21 +0200

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# jaimedevcode.github.io
+# CV Virtual | JaimeDevCode
+
+#### Técnico Superior de Desarrollo de Aplicaciones Multiplataformas
+
+### Es un currículum online en el que están estas secciones:
+
+- Breve información sobre mí
+- Educación y cursos
+- Experiencia laboral
+- Proyectos Web (Destacados y Freelance)
+- Conocimientos
+- Idiomas
+- Contacto
---
+++ db/db.js
@@ -0,0 +1,119 @@
+export default {
+    education: [
+    {
+      title: "Desarrollo de Aplicaciones Multiplataforma",
+      duration: "2022 - 2024",
+      subtitle: "IES Nervión, Sevilla",
+      details: [
+        "Desarrollo de aplicaciones multiplataforma, web y bases de datos. Programación en Java, JavaScript, Python, C#, XML, HTML, CSS y SQL.",
+      ],
+      tags: [
+        "Windows",
+        "Ubuntu",
+        "HTML",
+        "CSS",
+        "LESS",
+        "SASS",
+        "Bootstrap",
+        "Java",
+        "JavaScript",
+        "SQL",
+        "PHP",
+        "Python",
+        "C#",
+      ],
+      relevantSubjects: [
+        "Sistemas Informáticos",
+        "Programación",
+        "Programación de Servicios y Procesos",
+        "Acceso a Datos",
+        "Base de Datos",
+        "Programación Multimedia y Dispositivos Móviles"
+      ]
+    },
+    {
+      title: "Formación complementaria",
+      duration: "2022 - actualidad",
+      subtitle: "Google Coursera, El Rincon del Hacker...",
+      details: [
+        "Curso de Analisis de Datos - Google, curso de Fundamentos de ciberseguridad y hacking ético - El Rincón del Hacker,...",
+      ],
+      tags: [
+        "Linux",
+        "Windows",
+        "Kali Linux",
+        "Wireshark",
+        "Nmap",
+        "Metasploit",
+        "Cyberseguridad",
+        "Hacking ético",
+        "Análisis de datos",
+        "Google",
+        "Coursera",
+      ],
+    },
+  ],
+    projects: {
+    destacados: [
+      {
+        projectName: "Unscramble Pokemon Names 🎮 MC Mod",
+        image: "images/cv-unscramble.png",
+        summary:
+          "Mod de Minecraft 1.12 para descifrar el nombre de los Pokémon en el chat del juego.",
+        preview: "https://github.com/JaimeDevCode/UnscramblePokemonMod",
+        techStack: [
+          "Java",
+          "Minecraft Modding",
+          "Forge",
+          "GitHub",
+        ],
+      },
+    ],
+    freelance: [
+      {
+        projectName: "Unscramble Pokemon Names 🎮 MC Mod",
+        image: "images/cv-unscramble.png",
+        summary:
+          "Mod de Minecraft 1.12 para descifrar el nombre de los Pokémon en el chat del juego.",
+        preview: "https://github.com/JaimeDevCode/UnscramblePokemonMod",
+        techStack: [
+          "Java",
+          "Minecraft Modding",
+          "Forge",
+          "GitHub",
+        ],
+      },
+    ],
+  },
+  experience: [
+    {
+      title: "Alten Spain",
+      duration: "Abril. 2024 - actualidad",
+      subtitle: "Full Stack Developer",
+      details: [
+        "Desarrollo de aplicación web full-stack con Angular en frontend, Java en backend y PL/SQL Developer para gestión de bases de datos. Control de versiones y gestión de repositorios.",
+      ],
+      tags: [
+        "Angular",
+        "Node.js",
+        "PL/SQL",
+        "STS",
+        "TypeScript",
+      ],
+    },
+  ],
+  languages: [
+    {
+      title: "Spanish",
+      skillName: "Español (Nativo)",
+      color: "2",
+      percentage: "100",
+    },
+    {
+      title: "English",
+      skillName: "Inglés (B1 - B2)",
+      color: "1",
+      percentage: "70",
+    },
+  ],
+};
